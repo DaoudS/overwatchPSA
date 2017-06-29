ecfb5be0f1537576de357a8a7da170a7b9e49af5
DaoudS <daoud.sogoba@gmail.com>
2017-06-29 18:59:35 -0400

    Added first buttons for initial calculation

--- js/calculations.js
+++ js/calculations.js
@@ -1,5 +1,16 @@
-var Sens1 = document.getElementById("SensValue1");
+function initialCalc() {
+    var originalSens = document.getElementById("SensValue1").value;
 
-fucntion initialCalc(){
-    Sens1;
+    var newSensL = originalSens * .5;
+    var sensButton1 = document.createElement("BUTTON");
+    var sensButton1text = document.createTextNode(newSensL);
+    sensButton1.appendChild(sensButton1text);
+    document.body.appendChild(sensButton1);
+
+    var newSensH = originalSens * 1.5;
+    var sensButton2 = document.createElement("BUTTON");
+    var sensButton2text = document.createTextNode(newSensH);
+    sensButton2.appendChild(sensButton2text);
+    document.body.appendChild(sensButton2);  
+
 }
