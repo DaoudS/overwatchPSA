bc97fa2a87bd6c91e13f0ceb652e685a201d2b1c
DaoudS <daoud.sogoba@gmail.com>
2017-07-01 17:21:45 -0400

    added input vaildation for sensitivtiy

--- js/calculations.js
+++ js/calculations.js
@@ -1,3 +1,13 @@
+function validateInput(){
+    var x = document.getElementById("SensValue1").value;
+
+    if (x == "" || x <= 0) {
+        return false;
+    } else {
+        initialCalc();
+    }
+}
+
 function initialCalc() {
     var originalSens = document.getElementById("SensValue1").value;
 
@@ -5,17 +15,17 @@ function initialCalc() {
     var sensButton1 = document.createElement("BUTTON");
     var sensButton1text = document.createTextNode(newSensL);
     sensButton1.appendChild(sensButton1text);
-    document.body.appendChild(sensButton1);
+    document.getElementById("buttonSpot").appendChild(sensButton1);
 
     var newSensH = originalSens * 1.5;
     var sensButton2 = document.createElement("BUTTON");
     var sensButton2text = document.createTextNode(newSensH);
     sensButton2.appendChild(sensButton2text);
-    document.body.appendChild(sensButton2);  
+    document.getElementById("buttonSpot").appendChild(sensButton2);  
 
 }
 
-function secondCalc() {
+function calcButtton1() {
     var newSensL = newSensL * .5;
     sensButton1text.nodeValue = newSensL;
 } 
